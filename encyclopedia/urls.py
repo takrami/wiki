d9d50f909ba1848ed9e094998a6bd240f4b51bc1
Ati Takrami <ati.takrami@gmail.com>
2020-09-22 15:25:43 +0200

    intial edit page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("create", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,22 @@
 import markdown2
 
+
+from django import forms
 from django.shortcuts import render
 
 from . import util
 from markdown2 import Markdown
+
+markdowner = Markdown()
+
+
+class Search(forms.Form):
+    item = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Search'}))
+
+
+class Edit(forms.Form):
+    textarea = forms.CharField(widget=forms.Textarea(), label='')
 
 
 def index(request):
@@ -13,7 +26,6 @@ def index(request):
 
 
 def entry(request, entry):
-    markdowner = Markdown()
     entryPage = util.get_entry(entry)
     if entryPage is None:
         return render(request, "encyclopedia/nonExistingEntry.html", {
@@ -28,5 +40,11 @@ def entry(request, entry):
 
 def create(request):
     return render(request, "encyclopedia/create.html", {
+
+    })
+
+
+def edit(request, title):
+    return render(request, "encyclopedia/edit.html", {
 
     })
